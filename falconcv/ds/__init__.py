f7c3f46ae2cefa9b17086aa7df1bbcc41f85f8f4
Henry Ruiz <henryruiz22@gmail.com>
2020-03-25 23:54:06 -0500

    documentation changed

--- falconcv/ds/__init__.py
+++ falconcv/ds/__init__.py
@@ -4,3 +4,4 @@ from .openimages import OpenImages
 from .coco import Coco
 from .bing_scrapper import BingScrapper
 from .flickr_scrapper import FlickrScrapper
+from .image import  BoxRegion, PolygonRegion, TaggedImage
