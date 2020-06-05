9390978f77656b45fa20f93e9ab96c4c39815022
Henry Ruiz <henryruiz22@gmail.com>
2020-06-05 10:34:47 -0500

    fix:typo error at tf_trained.py file

--- falconcv/models/tf/tf_trained.py
+++ falconcv/models/tf/tf_trained.py
@@ -12,10 +12,11 @@ from falconcv.decor import typeassert, pathassert
 from falconcv.models.api_model import ApiModel
 from .misc import BoundingBox
 from .util import Utilities
-from .zoo import  ModelZoo
+from .zoo import ModelZoo
 from falconcv.util import ImageUtil
 import logging
-logger=logging.getLogger(__name__)
+
+logger = logging.getLogger(__name__)
 
 
 class TfTrainedModel(ApiModel):
@@ -26,104 +27,104 @@ class TfTrainedModel(ApiModel):
 
     def load_labels_map(self):
         assert os.path.isfile(self._labels_map), "Labels map file not found"
-        self._labels_map_dict=create_category_index_from_labelmap(self._labels_map,use_display_name=True)
+        self._labels_map_dict = create_category_index_from_labelmap(self._labels_map, use_display_name=True)
 
     def __enter__(self):
         self.load_labels_map()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_type:
-            logger.error("Error loading the model:  {}, {}".format(exc_type,str(exc_val)))
+            logger.error("Error loading the model:  {}, {}".format(exc_type, str(exc_val)))
 
     @staticmethod
-    def _process_input_image(input_image,size=None):
-        img_arr,scale_factor=ImageUtil.read(input_image),1  # read image
-        if size: img_arr,scale_factor=ImageUtil.resize(img_arr,width=size[0],height=[1])  # resize image
-        img_height,img_width=img_arr.shape[:2]
-        return img_arr,img_width,img_height,scale_factor
+    def _process_input_image(input_image, size=None):
+        img_arr, scale_factor = ImageUtil.read(input_image), 1  # read image
+        if size: img_arr, scale_factor = ImageUtil.resize(img_arr, width=size[0], height=[1])  # resize image
+        img_height, img_width = img_arr.shape[:2]
+        return img_arr, img_width, img_height, scale_factor
 
     @abc.abstractmethod
     @typeassert(input_image=np.ndarray)
     def output_dict(self, input_image: np.ndarray):
         raise NotImplementedError()
 
-    @typeassert(input_image=[str,np.ndarray],size=tuple,threshold=float,top_k=int)
-    def predict(self,input_image,size=None,threshold=0.5,top_k=10):
-        img_arr,img_width,img_height,scale_factor=self._process_input_image(input_image,size)
-        output_dict=self.output_dict(img_arr)
-        boxes=output_dict["detection_boxes"]
-        scores=output_dict["detection_scores"]
-        classes=output_dict["detection_classes"].astype(np.int64)
-        num_detections=output_dict['num_detections']
-        masks=[None for _ in range(num_detections)]
-        predictions=[]
+    @typeassert(input_image=[str, np.ndarray], size=tuple, threshold=float, top_k=int)
+    def predict(self, input_image, size=None, threshold=0.5, top_k=10):
+        img_arr, img_width, img_height, scale_factor = self._process_input_image(input_image, size)
+        output_dict = self.output_dict(img_arr)
+        boxes = output_dict["detection_boxes"]
+        scores = output_dict["detection_scores"]
+        classes = output_dict["detection_classes"].astype(np.int64)
+        num_detections = output_dict['num_detections']
+        masks = [None for _ in range(num_detections)]
+        predictions = []
         if 'detection_masks' in output_dict:
             # get masks
-            masks=output_dict["detection_masks"]
-            #adjust mask coordinates based on the images dimensions
-            masks=utils_ops.reframe_box_masks_to_image_masks(masks,boxes,img_height,img_width)
+            masks = output_dict["detection_masks"]
+            # adjust mask coordinates based on the images dimensions
+            masks = utils_ops.reframe_box_masks_to_image_masks(masks, boxes, img_height, img_width)
             # check eager execution mode
             if tf.executing_eagerly():
-                masks=tf.cast(masks > threshold,tf.uint8).numpy()
+                masks = tf.cast(masks > threshold, tf.uint8).numpy()
             else:
-                masks_tensor = tf.cast(masks > threshold,tf.uint8)
+                masks_tensor = tf.cast(masks > threshold, tf.uint8)
                 masks = masks_tensor.eval(session=tf.Session())
 
-        for box,mask,score,label in zip(boxes,masks,scores,classes):
+        for box, mask, score, label in zip(boxes, masks, scores, classes):
             if score >= threshold:
                 if self._labels_map_dict and label in self._labels_map_dict:
-                    label=self._labels_map_dict[label]["name"]
-                start_y,start_x,end_y,end_x=box
-                start_x=int(start_x*img_width)
-                start_y=int(start_y*img_height)
-                end_x=int(end_x*img_width)
-                end_y=int(end_y*img_height)
+                    label = self._labels_map_dict[label]["name"]
+                start_y, start_x, end_y, end_x = box
+                start_x = int(start_x * img_width)
+                start_y = int(start_y * img_height)
+                end_x = int(end_x * img_width)
+                end_y = int(end_y * img_height)
                 predictions.append(BoundingBox(
                     start_x,
                     start_y,
                     end_x,
                     end_y,
                     label,
-                    round(float(score),2),
+                    round(float(score), 2),
                     scale_factor,
                     mask
                 ))
         if len(predictions) > 0 and len(predictions) > top_k:
-            predictions=predictions[:top_k]
-        return cv2.cvtColor(img_arr,cv2.COLOR_BGR2RGB),predictions
+            predictions = predictions[:top_k]
+        return img_arr, predictions
 
 
 class TfFreezeModel(TfTrainedModel):
-    @typeassert(freeze_model=str,labels_map=str)
+    @typeassert(freeze_model=str, labels_map=str)
     @pathassert
-    def __init__(self,freeze_model: typing.Union[str, Path],labels_map: typing.Union[str, Path]):
+    def __init__(self, freeze_model: typing.Union[str, Path], labels_map: typing.Union[str, Path]):
         super(TfFreezeModel, self).__init__(labels_map)
-        self._freeze_model=freeze_model
-        _, ext=os.path.splitext(self._freeze_model)
+        self._freeze_model = freeze_model
+        _, ext = os.path.splitext(self._freeze_model)
         assert os.path.isfile(self._freeze_model) and ext == ".pb", "Invalid model file"
         self._graph = None
         self._session = None
 
     def __enter__(self):
-        super(TfFreezeModel,self).__enter__()
-        self._graph,self._session=Utilities.load_graph(self._freeze_model)
+        super(TfFreezeModel, self).__enter__()
+        self._graph, self._session = Utilities.load_graph(self._freeze_model)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        super(TfFreezeModel,self).__exit__(exc_type,exc_val,exc_tb)
+        super(TfFreezeModel, self).__exit__(exc_type, exc_val, exc_tb)
         if self._session:
             self._session.close()
 
     @staticmethod
     def _get_tensors_dict(graph):
-        tensors_list=[
+        tensors_list = [
             'num_detections',
             'detection_boxes',
             'detection_scores',
             'detection_classes',
             'detection_masks'
         ]
-        tensor_dict={}
+        tensor_dict = {}
         for opt in graph.get_operations():
             if opt.name in tensors_list:
                 tensor_dict[opt.name] = graph.get_tensor_by_name("{}:0".format(opt.name))
@@ -137,15 +138,15 @@ class TfFreezeModel(TfTrainedModel):
             output_dict = self._session.run(tensors_dict, feed_dict={image_tensor: img_expanded})
             num_detections = int(output_dict.pop('num_detections'))
             output_dict = {k: np.squeeze(v) for k, v in output_dict.items()}
-            output_dict['num_detections']=num_detections
+            output_dict['num_detections'] = num_detections
             return output_dict
         return None
 
 
 class TfSaveModel(TfTrainedModel):
-    @typeassert(model=str,labels_map=str)
+    @typeassert(model=str, labels_map=str)
     @pathassert
-    def __init__(self,model: typing.Union[str, Path],labels_map: typing.Union[str, Path]):
+    def __init__(self, model: typing.Union[str, Path], labels_map: typing.Union[str, Path]):
         super(TfSaveModel, self).__init__(labels_map)
         self._model = model
         self._tf_model = None
@@ -153,9 +154,9 @@ class TfSaveModel(TfTrainedModel):
     def __enter__(self):
         super(TfSaveModel, self).__enter__()
         if not os.path.isdir(self._model):
-            download_folder =ModelZoo.download_model(self._model)
+            download_folder = ModelZoo.download_model(self._model)
             self._model = os.path.join(download_folder, "saved_model")
-        self._tf_model=Utilities.load_save_model(self._model)
+        self._tf_model = Utilities.load_save_model(self._model)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -163,12 +164,12 @@ class TfSaveModel(TfTrainedModel):
 
     def output_dict(self, img_arr: np.ndarray):
         if self._tf_model:
-            img_expanded=np.expand_dims(img_arr,axis=0)
-            img_tensor=tf.convert_to_tensor(img_expanded)
-            output_dict=self._tf_model(img_tensor)  # just get the predictions of the model
+            img_expanded = np.expand_dims(img_arr, axis=0)
+            img_tensor = tf.convert_to_tensor(img_expanded)
+            output_dict = self._tf_model(img_tensor)  # just get the predictions of the model
             if tf.executing_eagerly():
-                num_detections=int(output_dict.pop('num_detections'))
-                output_dict={key: value[0,:num_detections].numpy() for key,value in output_dict.items()}
+                num_detections = int(output_dict.pop('num_detections'))
+                output_dict = {key: value[0, :num_detections].numpy() for key, value in output_dict.items()}
             else:
                 with tf.Session() as sess:
                     output_dict = sess.run(output_dict)
